76b6a6f739f4aca15a23b48bc2faa4b5dd83b617
Armin Ayari <armin.ayari@gmail.com>
2021-09-21 11:15:02 +0430

    feat: add absolute import using webpack alias

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
-import Page from "../../components/common/Page";
-import Header from "../../components/pages/Home/Header";
-import Content from "../../components/pages/Home/Content";
-import Footer from "../../components/pages/Home/Footer";
-import history from "../../utils/history";
-import string from "../../utils/string";
-import url from "../../utils/url";
+import Page from "@/components/common/Page";
+import Header from "@/components/pages/Home/Header";
+import Content from "@/components/pages/Home/Content";
+import Footer from "@/components/pages/Home/Footer";
+import history from "@/utils/history";
+import string from "@/utils/string";
+import url from "@/utils/url";
 
 export default function Home() {
   useEffect(() => {
